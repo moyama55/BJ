7da1c187b5e96530ba21c57f248e43d2fc5f39d3
moyama55 <moyama04@gmail.com>
2022-03-14 21:05:09 +0900

    third commit

--- BlackJack.java
+++ BlackJack.java
@@ -1,56 +1,46 @@
+import java.util.*;
 public class BlackJack{
-    /*以下の行から二種類のコメントアウトを使う
-        //! = その動作が何を表しているのかを説明している 
-        //? = 未完成部分のメソッドに何をさせたいのかを説明している    
-    */
     public static void main(String[] args){
-        //!自分の手札の枚数の合計
+        Scanner sc = new Scanner(System.in);
+        //自分の手札の枚数の合計
         int myhandSum = 0;
-        //!
         
-        //!自分の手札の値の合計
+        //自分の手札の値の合計
         int myValueSum = 0;  
-        //!
 
-        //!相手の手札の枚数の合計
+        //相手の手札の枚数の合計
         int enemyhandSum = 0;
-        //!
 
-        //!相手の手札の値の合計
+        //相手の手札の値の合計
         int enemyValueSum = 0;  
-        //!
         
-        //!トランプを表している配列
-        int[] cards = new int[51];
-        //!
-        
-        //!ゲーム開始時メッセージ
         System.out.println("ようこそブラックジャックの世界へ");
         System.out.println("ゲームを開始します");
-        //!
-        
         System.out.println("あなたの手札は");
         
-        //?トランプを表す配列からランダムに一つとる
-        //!自分の手札        
+        //トランプを表す配列からランダムに一つとるのを敵味方で４回繰り返す
+        
+        //自分の手札        
         System.out.print("自分の手札: ");
         System.out.print(myhandSum + "枚目");
         System.out.println(myhandSum + 1 +"枚目");
-        //!相手の手札
+        //相手の手札
         System.out.print("相手の手札: ");
-        System.out.print(enemyhandSum + 1 +"枚目");
-        System.out.println(enemyhandSum + 1 + "枚目");
-        //?
+        System.out.print(enemyhandSum +"枚目");
         
-        //?自分の手札ドロー
+        //自分の手札ドロー
         System.out.print("自分の手札: ");
-        
-        //?    
-        
-        //?１枚目の手札と２枚目の手札の合計が21以上の場合バーストをしたとみなる
+        System.out.println("次のカードを引きますか？:");
+        System.out.println("カードを引く場合はyesをそのままの場合はnoを書いてください");
+        String drawCard = sc.next();
+        if(drawCard == "yes"){
+            //カードを引ける処理
+        }else if(drawCard == "no"){
+            //そのまま続行する処理
+        }
+        //１枚目の手札と２枚目の手札の合計が21以上の場合バーストをしたとみなる
         if(myValueSum> 21){
             System.out.print("バーストしました。ゲームを終了します");
         }
-        //?
     }
 }
---
+++ TranpDeck.java
@@ -0,0 +1,8 @@
+public class TranpDeck {
+    //トランプを表している配列
+    int[] decks = new int[51];
+    int count = 0;
+    public void deck(){
+        for()    
+    }
+}
